5eb09702af1f72e1cc9ece1677e90a9f3d360e60
MJ <MJesus.duenas1@alu.uclm.es>
2023-03-11 20:57:59 +0100

    artist and artistDAO  classes developed without testing

---
+++ SetList/Domain/Artist.vb
@@ -0,0 +1,94 @@
+﻿Public Class Artist
+    Private Property IdArtist As Integer
+    Private Property aName As String
+    Private Property country As String
+    Private Property image As String
+    Private ReadOnly Property artistsDAO As ArtistDAO
+    Private User As String
+    Private favDate As Date
+    Public Sub New()
+        Me.artistsDAO = New ArtistDAO
+    End Sub
+    Public Sub New(id As Integer)
+        Me.artistsDAO = New ArtistDAO
+        Me.IdArtist = id
+    End Sub
+    Public Sub New(id As Integer, name As String, country As String, image As String)
+        Me.artistsDAO = New ArtistDAO
+        IdArtist = id
+        aName = name
+        Me.country = country
+        Me.image = image
+    End Sub
+    Public Sub New(user As String, IdArtist As Integer, favDate As Date)
+        Me.artistsDAO = artistsDAO
+        Me.User = user
+        Me.IdArtist = IdArtist
+        Me.favDate = favDate
+    End Sub
+    Public Sub ReadArtist()
+        Me.artistsDAO.Read(Me)
+    End Sub
+    Public Function GetIdArtist() As Integer
+        Return IdArtist
+    End Function
+    Public Function GetImage() As String
+        Return image
+    End Function
+    Public Function GetCountry() As String
+        Return country
+    End Function
+    Public Function GetName() As String
+        Return aName
+    End Function
+    Public Function GetUser() As String
+        Return User
+    End Function
+    Public Function GetFavDate() As Date
+        Return favDate
+    End Function
+    Public Function SetUser(user As String) As String
+        Me.User = user
+    End Function
+    Public Function SetFavDate(favDate As Date) As Date
+        Me.favDate = favDate
+    End Function
+    Public Function setIdArtist(idArtsit As Integer) As Integer
+        IdArtist = idArtsit
+    End Function
+    Public Function SetName(name As String) As String
+        aName = name
+    End Function
+    Public Function SetImage(image As String) As String
+        Me.image = image
+    End Function
+    Public Function SetCountry(country As String) As String
+        Me.country = country
+    End Function
+    Public Function ReadAllFavArtists(path As String) As Object
+        Return Me.artistsDAO.ReadAllFavArtists(path)
+    End Function
+    Public Function ReadAllArtists(path As String) As Object
+        Return Me.artistsDAO.ReadAll(path)
+    End Function
+    Public Sub ReadAll()
+        Me.artistsDAO.Read(Me)
+    End Sub
+    Public Function InsertArtist() As Integer
+        Return Me.artistsDAO.Insert(Me)
+    End Function
+    Public Function InsertFav_Artist() As Integer
+        Return Me.artistsDAO.InsertFav(Me)
+    End Function
+    Public Function DeleteFav_Artist() As Integer
+        Return Me.artistsDAO.DeleteFav_Artist(Me)
+    End Function
+    Public Function UpdateArtist() As Integer
+        Return Me.artistsDAO.Update(Me)
+    End Function
+    Public Function DeleteArtist() As Integer
+        Return Me.artistsDAO.Delete(Me)
+    End Function
+
+
+End Class
--- SetList/Domain/artistDAO.vb
+++ SetList/Domain/artistDAO.vb
@@ -1,3 +1,64 @@
 ﻿Public Class ArtistDAO
+    Public ReadOnly Property Artists As Collection
+
+    Public Sub New()
+        Me.Artists = New Collection
+    End Sub
+
+    Public Function ReadAll(path As String) As Object
+        Dim ar As Artist
+        Dim col, aux As Collection
+        col = DBBroker.GetBroker().Read("SELECT * FROM Artists ORDER BY IdArtist")
+        For Each aux In col
+            ar = New Artist(CInt(aux(1).ToString))
+            ar.SetName(aux(2).ToString)
+            ar.SetCountry(aux(3).ToString)
+            ar.SetImage(aux(4).ToString)
+            Me.Artists.Add(ar)
+        Next
+        Return Artists
+    End Function
+
+    Public Function ReadAllFavArtists(path As String) As Object
+        Dim ar As Artist
+        Dim fa As New Collection
+        Dim col, aux As Collection
+        col = DBBroker.GetBroker().Read("SELECT * FROM Fav_Artists ORDER BY user")
+        For Each aux In col
+            ar = New Artist()
+            ar.SetUser(aux(1).ToString)
+            ar.setIdArtist(CInt(aux(2).ToString))
+            ar.SetFavDate(CDate(aux(3).ToString))
+            fa.Add(ar)
+        Next
+        Return fa
+    End Function
+
+    Public Sub Read(ByRef ar As Artist)
+        Dim col As Collection : Dim aux As Collection
+        col = DBBroker.GetBroker.Read("SELECT * FROM Artists WHERE IdArtist='" & ar.GetIdArtist() & "';")
+        For Each aux In col
+            ar.SetName(aux(2).ToString)
+            ar.SetCountry(aux(3).ToString)
+            ar.SetImage(aux(4).ToString)
+        Next
+    End Sub
+
+    Public Function InsertFav(ByVal s As Artist) As Integer
+        Return DBBroker.GetBroker.Change("INSERT INTO Fav_Artists VALUES ('" & s.GetUser() & "', '" & s.GetIdArtist() & "', '" & s.GetFavDate() & "');")
+    End Function
+    Public Function Insert(ByVal ar As Artist) As Integer
+        Return DBBroker.GetBroker.Change("INSERT INTO Artists ([aName],country,[image]) VALUES ('" & ar.GetName() & "','" & ar.GetCountry() & "','" & ar.GetImage() & "');")
+    End Function
+    Public Function Update(ByVal ar As Artist) As Integer
+        Return DBBroker.GetBroker.Change("UPDATE Artists SET aName='" & ar.GetName() & "' ,country='" & ar.GetCountry() & "' ,[image]='" & ar.GetImage() & "'WHERE IdArtist=" & ar.GetIdArtist() & ";")
+    End Function
+    Public Function Delete(ByVal ar As Artist) As Integer
+        Return DBBroker.GetBroker.Change("DELETE FROM Artists WHERE IdArtist=" & ar.GetIdArtist() & ";")
+    End Function
+    Public Function DeleteFav_Artist(ByVal ar As Artist) As Integer
+        Return DBBroker.GetBroker.Change("DELETE FROM Fav_Artists WHERE artist=" & ar.GetIdArtist() & ";")
+    End Function
+
 
 End Class
