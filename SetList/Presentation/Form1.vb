28c4e55a5e94759a7725c28e10f21d0353214919
AndresGonzalez23 <gonzalezandres010@gmail.com>
2023-03-16 15:03:20 +0100

    Cleared Classes

--- SetList/Presentation/Form1.vb
+++ SetList/Presentation/Form1.vb
@@ -4,8 +4,5 @@ Imports System.Windows.Forms.VisualStyles.VisualStyleElement.ToolBar
 Public Class Form1
     Private Sub btn_connection_Click(sender As Object, e As EventArgs) Handles btn_connection.Click
 
-        Dim main As New Main 'Crea una nueva instancia de Form2
-        main.Show() 'Muestra Form2
-
     End Sub
 End Class
