bdec69495b9049a76e3d1df8c1f626183d283b71
AndresGonzalez23 <gonzalezandres010@gmail.com>
2023-04-13 11:49:18 +0200

    Setlist update and delete no testing

--- SetList/Domain/SetlistDAO.vb
+++ SetList/Domain/SetlistDAO.vb
@@ -8,4 +8,10 @@
     Public Function Insert(ByVal setlist As Setlist) As Integer
         Return DBBroker.GetBroker.Change("INSERT INTO setlists (Concert,Song,OrderInSetlist) VALUES (" & setlist.setlistConcerts & "," & setlist.setlistSongs & "," & setlist.OrderInSetlist & ");")
     End Function
+    Public Function Update(ByVal setlist As Setlist) As Integer
+        Return DBBroker.GetBroker.Change("UPDATE setlists SET Concert=" & setlist.setlistConcerts & " ,Song=" & setlist.setlistSongs & " ,OrderInSetList=" & setlist.OrderInSetlist & "WHERE Concert=" & setlist.setlistConcerts & ";")
+    End Function
+    Public Function Delete(ByVal setlist As Setlist) As Integer
+        Return DBBroker.GetBroker.Change("DELETE FROM setlists WHERE Concert=" & setlist.setlistConcerts & " AND Song=" & setlist.setlistSongs & ";")
+    End Function
 End Class
