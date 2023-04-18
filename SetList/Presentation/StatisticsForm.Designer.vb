20c988c5a9b3616c6d7cfc835a8f5467e41bb360
MJ <MJesus.duenas1@alu.uclm.es>
2023-04-18 11:10:31 +0200

    usable statisticForm

--- SetList/Presentation/StatisticsForm.Designer.vb
+++ SetList/Presentation/StatisticsForm.Designer.vb
@@ -36,7 +36,7 @@ Partial Class StatisticsForm
         Me.lst_artists.Font = New System.Drawing.Font("Verdana", 10.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.lst_artists.FormattingEnabled = True
         Me.lst_artists.ItemHeight = 20
-        Me.lst_artists.Location = New System.Drawing.Point(22, 11)
+        Me.lst_artists.Location = New System.Drawing.Point(218, 86)
         Me.lst_artists.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
         Me.lst_artists.Name = "lst_artists"
         Me.lst_artists.Size = New System.Drawing.Size(150, 124)
@@ -44,26 +44,30 @@ Partial Class StatisticsForm
         '
         'lst_moreSinged
         '
+        Me.lst_moreSinged.Font = New System.Drawing.Font("Verdana", 10.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.lst_moreSinged.FormattingEnabled = True
-        Me.lst_moreSinged.ItemHeight = 16
-        Me.lst_moreSinged.Location = New System.Drawing.Point(619, 11)
+        Me.lst_moreSinged.ItemHeight = 20
+        Me.lst_moreSinged.Location = New System.Drawing.Point(387, 86)
         Me.lst_moreSinged.Name = "lst_moreSinged"
-        Me.lst_moreSinged.Size = New System.Drawing.Size(150, 132)
+        Me.lst_moreSinged.Size = New System.Drawing.Size(150, 124)
         Me.lst_moreSinged.TabIndex = 11
         '
         'Label1
         '
         Me.Label1.AutoSize = True
-        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label1.Location = New System.Drawing.Point(215, 11)
+        Me.Label1.BackColor = System.Drawing.Color.Transparent
+        Me.Label1.Font = New System.Drawing.Font("Verdana", 10.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.ForeColor = System.Drawing.SystemColors.ButtonHighlight
+        Me.Label1.Location = New System.Drawing.Point(53, 24)
         Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(346, 40)
+        Me.Label1.Size = New System.Drawing.Size(315, 60)
         Me.Label1.TabIndex = 12
-        Me.Label1.Text = "Songs performed the most times by an artist " & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "throughout all his/her concerts"
+        Me.Label1.Text = "Songs performed the most times" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "by an artist  throughout all his/her " & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "concerts"
         '
         'btn_searchSql1
         '
-        Me.btn_searchSql1.Location = New System.Drawing.Point(447, 82)
+        Me.btn_searchSql1.Font = New System.Drawing.Font("Verdana", 10.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btn_searchSql1.Location = New System.Drawing.Point(57, 157)
         Me.btn_searchSql1.Name = "btn_searchSql1"
         Me.btn_searchSql1.Size = New System.Drawing.Size(114, 41)
         Me.btn_searchSql1.TabIndex = 13
@@ -72,18 +76,19 @@ Partial Class StatisticsForm
         '
         'lst_songs
         '
-        Me.lst_songs.Font = New System.Drawing.Font("Verdana", 10.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lst_songs.Font = New System.Drawing.Font("Verdana", 7.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.lst_songs.FormattingEnabled = True
-        Me.lst_songs.ItemHeight = 20
-        Me.lst_songs.Location = New System.Drawing.Point(159, 236)
+        Me.lst_songs.ItemHeight = 16
+        Me.lst_songs.Location = New System.Drawing.Point(733, 66)
         Me.lst_songs.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
         Me.lst_songs.Name = "lst_songs"
-        Me.lst_songs.Size = New System.Drawing.Size(550, 144)
+        Me.lst_songs.Size = New System.Drawing.Size(581, 164)
         Me.lst_songs.TabIndex = 14
         '
         'btn_Query4
         '
-        Me.btn_Query4.Location = New System.Drawing.Point(22, 268)
+        Me.btn_Query4.Font = New System.Drawing.Font("Verdana", 10.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btn_Query4.Location = New System.Drawing.Point(1200, 235)
         Me.btn_Query4.Name = "btn_Query4"
         Me.btn_Query4.Size = New System.Drawing.Size(114, 41)
         Me.btn_Query4.TabIndex = 15
@@ -93,20 +98,22 @@ Partial Class StatisticsForm
         'Label2
         '
         Me.Label2.AutoSize = True
-        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label2.Location = New System.Drawing.Point(18, 165)
+        Me.Label2.BackColor = System.Drawing.Color.Transparent
+        Me.Label2.Font = New System.Drawing.Font("Verdana", 10.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.ForeColor = System.Drawing.SystemColors.ButtonHighlight
+        Me.Label2.Location = New System.Drawing.Point(729, 24)
         Me.Label2.Name = "Label2"
-        Me.Label2.Size = New System.Drawing.Size(296, 60)
+        Me.Label2.Size = New System.Drawing.Size(371, 40)
         Me.Label2.TabIndex = 16
-        Me.Label2.Text = "List with the complete Information " & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "of the songs that have been performed" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & " the " &
-    "most times"
+        Me.Label2.Text = "Complete Information of the songs " & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "that have been performed the most times"
         '
         'StatisticsForm
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.BackgroundImage = Global.SetList.My.Resources.Resources.Five_Simple_Editing_Tips_That_Will_Make_Your_Photos_Stand_Out
-        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.ClientSize = New System.Drawing.Size(1382, 753)
         Me.Controls.Add(Me.Label2)
         Me.Controls.Add(Me.btn_Query4)
         Me.Controls.Add(Me.lst_songs)
